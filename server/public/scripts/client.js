c9cc5a71a020042e8c95ee9e188e7bf90b92e65b
Thy696 <thyvu411@gmail.com>
2020-04-26 12:48:20 -0500

    using Sweet Alert to styling for option

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -27,6 +27,7 @@ function addClick() {
         data: taskToSend
     }).then((response) => {
         console.log('Back from POST', response);
+        swal("Added new task!", ".", "success");
         getItem();
         $('#inputTask').val('');
     }).catch((error) => {
@@ -69,20 +70,39 @@ function deleteTask() {
     console.log('deleteTask clicked');
     let taskId = $(this).data('id');
     console.log(`in DELETE ${taskId}`, taskId);
-    let confirmDelete = confirm('Are you sure want to delete this task?');
-    if (confirmDelete == true) {
-        $.ajax({
+    // let confirmDelete = confirm('Are you sure want to delete this task?');
+     // if (confirmDelete == true) { 
+    // } else {
+    //     return false;
+    // }
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this task!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Poof! Your task has been deleted!", {
+            icon: "success",
+          });
+          $.ajax({
             type: 'DELETE',
             url: `/tasks/${taskId}`,
         }).then((response) => {
             console.log(response);
+            
             getItem();
         }).catch((error) => {
             console.log('Error in delete task', error);
         })
-    } else {
-        return false;
-    }
+        } else {
+          swal("Your task is safe!");
+        }
+      });
+
+   
 }
 
 //-----------------------------------------------------

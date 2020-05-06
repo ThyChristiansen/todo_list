7e72eea2553a2bf6c72c7a708a388235476645ed
Thy696 <thyvu411@gmail.com>
2020-05-05 23:30:32 -0500

    change screen shot picture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -68,7 +68,7 @@ function renderToDOM(tasksArray) {
         el.append(row); //append tasks
 
         //delete icon
-        row.append(`<td><svg class="bi bi-trash-fill" id = "deleteBtn" data-id = ${task.id} width="1em" height="1em" viewBox="0 0 16 16" fill="red" xmlns="http://www.w3.org/2000/svg">
+        row.append(`<td><svg class="bi bi-trash-fill" id = "deleteBtn" data-id = ${task.id} width="1em" height="1em" viewBox="0 0 16 16" fill="#a72f1d" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z" clip-rule="evenodd"/>
       </svg></td>`)
     }
